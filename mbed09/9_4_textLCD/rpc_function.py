0a26c746aebeec135c1a12a4dd3b70a77c52308b
TrangDuLam <andyhardaway07@gmail.com>
2021-04-21 03:00:11 -0700

    master

---
+++ mbed09/9_4_textLCD/rpc_function.py
@@ -0,0 +1,40 @@
+import serial
+import time
+serdev = '/dev/ttyACM0'
+s = serial.Serial(serdev, 9600)
+
+s.write(bytes("\r", 'UTF-8'))
+line=s.readline() # Read an echo string from mbed terminated with '\n' (putc())
+print(line)
+line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC reply)
+print(line)
+time.sleep(1)
+
+s.write(bytes("/doDisplay/run I'M \r", 'UTF-8'))
+line=s.readline() # Read an echo string from mbed terminated with '\n' (putc())
+print(line)
+line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC reply)
+print(line)
+time.sleep(1)
+
+
+s.write(bytes("/doLocate/run 4 0\r", 'UTF-8'))
+line=s.readline() # Read an echo string from mbed terminated with '\n' (putc())
+print(line)
+line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC reply)
+print(line)
+time.sleep(1)
+
+s.write(bytes("/doDisplay/run 106061121\r", 'UTF-8'))
+line=s.readline() # Read an echo string from mbed terminated with '\n' (putc())
+print(line)
+line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC reply)
+print(line)
+time.sleep(1)
+
+s.write(bytes("\r", 'UTF-8'))
+line=s.readline() # Read an echo string from mbed terminated with '\n' (putc())
+print(line)
+line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC reply)
+print(line)
+time.sleep(1)
